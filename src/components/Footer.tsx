cf5a1d8dbeffe7513c3d360eafe1961f10879cc2
Ntamas <manolis.ntamadakis@gmail.com>
2024-11-29 16:21:42 +0200

    Small fixes that will included on the next version

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const Footer: React.FC = () => {
   return (
     <a
-      href="ntamadakis.gr"
+      href="https://ntamadakis.gr"
+      target="_blank"
+      rel="noopener noreferrer"
       className="bg-teal-500 p-4 rounded-md shadow-md text-sm text-gray-800 font-bold text-center"
     >
       Powered by Manolis Ntamadakis
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,11 @@ import React from "react";
 const Header: React.FC = () => {
   return (
     <header className="bg-white p-4 rounded-md shadow-md flex flex-row justify-between items-center">
-      <a href="#">
+      <a
+        href="https://ntamadakis.gr/projects/project/website-pintabs-organizer-by-ntamas"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <img
           src="logo/Logo128.png"
           alt="Logo"
